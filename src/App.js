3d5b461d1cc227d321d3566c0dd355159cd30c3a
Abhidha Dixit <121662241+abhidhadixit@users.noreply.github.com>
2023-11-07 23:42:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Colourpick from "./Components/Colorpick";
+import "./App.css";
+import Word from "./Components/Word";
 function App() {
   return (
-    <div class="background d-flex flex-column justify-content-center align-items-center">
-      <h1 class="text-center h1 fw-bold">Color Picker</h1>
-      <Colourpick />
+    <div>
+      <Word />
     </div>
   );
 }
